ebde83b0477bdc3bc573d1896ecc6f977536c9f9
Simon <ashingtonsid@gmail.com>
2018-12-27 13:33:39 +0000

    Added single line extraction of object attrs into lists

--- list_of_lists_from_list.py
+++ list_of_lists_from_list.py
@@ -1,17 +1,27 @@
 from myPackages.nima_emulation import nima_row
-import uuid
-import random
+import uuid, random
 
+NIMA_ROWS = []
 
 
 def main():
     # Create a list of rows
-    rows = [ nima_row(uuid.uuid4(), 
+    NIMA_ROWS = [ nima_row(uuid.uuid4(), 
                       random.randint(50,100)/2,
                       str(random.randint(100,200)),
                       random.randint(1,10)) for i in range(1,21) ]
-    print(*rows, sep='\n')
+    print(*NIMA_ROWS, sep='\n')
 
+    # Get a list of identifiers
+    the_list_of_lists = list(zip(*((row.identifier, row.float_val, row.string_val, row.int_val) for row in NIMA_ROWS)))
+
+    ids = the_list_of_lists[0]
+    floats = the_list_of_lists[1]
+    strings = the_list_of_lists[2]
+    ints = the_list_of_lists[3]
+
+    print(floats)
+    print(type(floats))
 
 
 
